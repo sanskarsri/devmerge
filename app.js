a4608aa198893a649be3b9101dab1e0cd708b9e3
Sanskar Srivastava <sanskar0703@gmail.com>
2022-10-09 17:17:13 +0530

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+function splitScroll(){
+    const controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({
+        duration:'200%',
+        triggerElement:'.about-title',
+        triggerHook:0
+    })
+    .setPin('.about-title')
+    // .addIndicators()
+    .addTo(controller);
+}
+
+splitScroll();
